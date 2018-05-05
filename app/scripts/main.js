936e76c6862b77f276910a2ad198dda9145bb0da
cuilei5205189 <allen5205189@gmail.com>
2018-05-05 17:34:15 +0800

    故事区完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,45 +1,68 @@
-(function() {
-  'use strict';
-  // Your custom JavaScript goes here
-  
-  // navbar
-  $('.ui.navbar .search-icon > i')
-    .click(function () {
-      var icon = $(this)
-      if (icon.text() === 'search') {
-        icon.text('close').parents('.content').addClass('searching')
-      } else {
-        icon.text('search').parents('.content').removeClass('searching')
-        }
+(function () {
+	'use strict';
+	// Your custom JavaScript goes here
+
+	// navbar
+	$('.ui.navbar .search-icon > i')
+		.click(function () {
+			var icon = $(this)
+			if (icon.text() === 'search') {
+				icon.text('close').parents('.content').addClass('searching')
+			} else {
+				icon.text('search').parents('.content').removeClass('searching')
+			}
+		})
+
+	//sidebar
+	$('.ui.sidebar')
+		.sidebar('setting', 'dimPage', false)
+		.sidebar('attach events', ' .menu-icon')
+		.sidebar('attach events', ' .close-icon')
+
+	$('.ui.navbar .menu')
+		.clone()
+		.appendTo('.ui.sidebar')
+
+	//slick carousel
+	$('.ui.hero.carousel')
+		.slick({
+			autoplay: true,
+			autoplaySpeed: 2500,
+			arrows: true,
+			prevArrow: '<button class="ui prev button bottom"><i class="material-icons">chevron_left</i></button>',
+			nextArrow: '<button class="ui next button bottom"><i class="material-icons">chevron_right</i></button>',
+
+			responsive: [{
+				breakpoint: 767,
+				settings: {
+					dots: true
+				}
+			}]
+		});
+
+  $('.ui.vertical.story .ui.carousel.for')
+    .slick({
+      asNavFor: '.ui.vertical.story .ui.carousel.nav',
+      arrows: false,
+      slidesToShow: 1,
     })
 
-  //sidebar
-  $('.ui.sidebar')
-    .sidebar('setting', 'dimPage', false)
-    .sidebar('attach events', ' .menu-icon')
-    .sidebar('attach events', ' .close-icon')
-
-  $('.ui.navbar .menu')
-    .clone()
-    .appendTo('.ui.sidebar')
-
-  //slick carousel
-  $('.ui.hero.carousel')
+  $('.ui.vertical.story .ui.carousel.nav')
     .slick({
-      autoplay: true,
-      autoplaySpeed: 2500,
-      arrows: true,
-      prevArrow : '<button class="ui prev button bottom"><i class="material-icons">chevron_left</i></button>',
-      nextArrow : '<button class="ui next button bottom"><i class="material-icons">chevron_right</i></button>',
-
+      asNavFor: '.ui.vertical.story .ui.carousel.for',
+      slidesToShow: 3,
+      dots: true,
+      prevArrow: '<button class="ui prev button"><i class="material-icons">chevron_left</i></button>',
+      nextArrow: '<button class="ui next button"><i class="material-icons">chevron_right</i></button>',
+      centerMode: true,
       responsive: [
         {
           breakpoint: 767,
           settings: {
-            dots: true
+            slidesToShow: 1
           }
         }
-      ]
-    });
-})();
+      ],
+    })
 
+})();
