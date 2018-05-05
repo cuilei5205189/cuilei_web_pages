a3ac7ca2a2d48f09fc89516a2b2eb3075e818538
cuilei5205189 <allen5205189@gmail.com>
2018-05-06 00:06:08 +0800

    产品页样式调整

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,6 +47,49 @@
 			slidesToShow: 1,
 		})
 
+	$('#exterior-design .carousel.text')
+		.slick({
+			asNavFor: '#exterior-design .carousel.image',
+			arrows: false,
+			slidesToShow: 1,
+		})
+
+	$('#exterior-design .carousel.image')
+		.slick({
+			asNavFor: '#exterior-design .carousel.text',
+			slidesToShow: 2,
+			dots: true,
+			prevArrow: '<button class="ui prev button"><i class="material-icons">chevron_left</i></button>',
+			nextArrow: '<button class="ui next button"><i class="material-icons">chevron_right</i></button>',
+			responsive: [{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1
+				}
+			}],
+		})
+	$('#interior-design .carousel.text')
+		.slick({
+			asNavFor: '#interior-design .carousel.image',
+			arrows: false,
+			slidesToShow: 1,
+		})
+
+	$('#interior-design .carousel.image')
+		.slick({
+			asNavFor: '#interior-design .carousel.text',
+			slidesToShow: 1,
+			dots: true,
+			prevArrow: '<button class="ui prev button"><i class="material-icons">chevron_left</i></button>',
+			nextArrow: '<button class="ui next button"><i class="material-icons">chevron_right</i></button>',
+			responsive: [{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1
+				}
+			}],
+		})
+
 	$('.ui.vertical.story .ui.carousel.nav')
 		.slick({
 			asNavFor: '.ui.vertical.story .ui.carousel.for',
